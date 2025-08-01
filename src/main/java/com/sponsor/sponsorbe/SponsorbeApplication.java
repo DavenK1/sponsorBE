4f1398959f2212f9404d440723ecb67e6c174378
DavenK1 <daven.kaufmann@gmail.com>
2025-07-31 20:16:45 -0400

    added spring

---
+++ src/main/java/com/sponsor/sponsorbe/SponsorbeApplication.java
@@ -0,0 +1,13 @@
+package com.sponsor.sponsorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsorbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SponsorbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sponsor/sponsorbe/SponsorbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sponsor.sponsorbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SponsorbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
